3bfb722d85e409b357d2eef9cbd795e71db047b1
mike smith <mikejsmith9843@gmail.com>
2024-08-10 13:04:00 -0400

    Server settings

--- server/utils/seed.js
+++ server/utils/seed.js
@@ -114,7 +114,7 @@ async function seedDb() {
 
 async function connectAndSeed() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/', {
+        await mongoose.connect('mongodb://127.0.0.1:27017/', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
